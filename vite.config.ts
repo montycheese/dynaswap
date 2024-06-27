d8835b88b19c27e3bf301295835484490b039209
Montana Wong <montana@dynamic.xyz>
2024-06-27 14:47:08 -0400

    add polyfills for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
- import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/reactjs-template',
+  optimizeDeps: {
+    esbuildOptions: {
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                process: false,
+            }),
+            NodeModulesPolyfillPlugin(),
+        ],
+    },
+  },
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
