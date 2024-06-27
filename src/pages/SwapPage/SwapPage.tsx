cadf273afa0441691ed6fd7891dd8b0c722bac83
Montana Wong <montana@dynamic.xyz>
2024-06-27 17:56:18 -0400

    update swap widget theme

--- src/pages/SwapPage/SwapPage.tsx
+++ src/pages/SwapPage/SwapPage.tsx
@@ -2,6 +2,22 @@ import {FC, useEffect, useState} from "react";
 import {DynamicWidget, useDynamicContext} from "@dynamic-labs/sdk-react-core";
 import { Widget } from "@kyberswap/widgets";
 
+const theme = {
+    primary: '#1C1C1C',
+    secondary: '#0F0F0F',
+    dialog: '#313131',
+    borderRadius: '20px',
+    buttonRadius: '24px',
+    stroke: '#505050',
+    interactive: '#292929',
+    accent: '##28E0B9',
+    success: '189470',
+    warning: 'FF9901',
+    error: 'FF537B',
+    text: '#FFFFFF',
+    subtext: 'A9A9A9',
+    fontFamily: 'Work Sans',
+}
 export const SwapPage: FC = () => {
     const { primaryWallet, network, sdkHasLoaded } = useDynamicContext();
     const [provider, setProvider] = useState(null);
@@ -30,6 +46,7 @@ export const SwapPage: FC = () => {
                 enableDexes="kyberswap-elastic,uniswapv3,uniswap"
                 provider={provider}
                 title={<div>Dynaswap</div>}
+                theme={theme}
             />
         </div>
     );
